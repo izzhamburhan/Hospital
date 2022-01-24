da53b28eff8a569266ec1d6d89237dc317a822bd
izzhamburhan <maizzham01@gmail.com>
2022-01-24 16:58:37 +0800

    plus

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -84,12 +84,21 @@
             </li>
             
             <!-- login and register button -->
+            @if(Route::has('login'))
+            @auth
+
+            <h1>user is logged in !</h1>
+
+            @else
             <li class="nav-item">
               <a class="btn btn-primary ml-lg-3" href="{{ route('login' )}}">Login</a>
             </li>
             <li class="nav-item">
               <a class="btn btn-primary ml-lg-3" href="{{ route('register' )}}">Register</a>
             </li>
+            @endauth
+
+            @endif
 
 
           </ul>
